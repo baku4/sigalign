16e832f572d6ff67e33642d6c3e026e2c823bd0e
baku4 <bahkhun@gmail.com>
2022-08-25 13:50:46 +0900

    Change fasta file reader to use seq_io

--- sigalign/src/util/file_reader.rs
+++ sigalign/src/util/file_reader.rs
@@ -4,31 +4,29 @@ use std::path::Path;
 use std::{fs::read, io::{Read, Write, BufReader}};
 use std::fs::File;
 
-use bio::io::{fasta, fastq};
-use bio::io::fasta::IndexedReader;
+use seq_io::fasta::{
+    Reader as SeqIoReader,
+    Record,
+};
 
 pub struct FastaReader<R: Read> {
-    records: fasta::Records<BufReader<R>>
+    reader: SeqIoReader<R>,
 }
 
 impl<R: Read> FastaReader<R> {
     pub fn new(reader: R) -> Self {
-        let reader = fasta::Reader::new(reader);
-
+        let reader = SeqIoReader::new(reader);
         Self {
-            records: reader.records()
+            reader
         }
     }
 }
 impl FastaReader<File> {
-    pub fn from_file_path<P: AsRef<Path> + std::fmt::Debug>(file_path: P) -> Result<Self> {
-        let reader = fasta::Reader::from_file(file_path)?;
-
-        Ok(
-            Self {
-                records: reader.records()
-            }
-        )
+    pub fn from_file_path<P: AsRef<Path>>(file_path: P) -> Result<Self> {
+        let reader = SeqIoReader::from_path(file_path)?;
+        Ok(Self {
+            reader
+        })
     }
 }
 impl<'a> FastaReader<&'a [u8]> {
@@ -36,26 +34,20 @@ impl<'a> FastaReader<&'a [u8]> {
         Self::new(bytes)
     }
 }
-
-// TODO: Change fasta reader to new one
 impl<R: Read> Iterator for FastaReader<R> {
     type Item = (String, Vec<u8>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.records.next() {
-            Some(record) => {
-                match record {
-                    Ok(record) => {
-                        Some((record.id().to_string(), record.seq().to_vec()))
-                    },
-                    Err(_) => {
-                        None
-                    }
-                }
+        match self.reader.next() {
+            Some(Ok(record)) => {
+                Some((
+                    String::from_utf8(record.id_bytes().to_vec()).unwrap(),
+                    record.to_owned_record().seq,
+                ))
             },
-            None => {
+            _ => {
                 None
-            }
+            },
         }
     }
 }
