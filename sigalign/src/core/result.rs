4b858eb6ea0f6742d6343c1156767eec5d8b9e27
baku4 <bahkhun@gmail.com>
2022-09-20 21:35:56 +0900

    Debug deserialization json for result of short_key feature

--- sigalign/src/core/result.rs
+++ sigalign/src/core/result.rs
@@ -1,108 +1,63 @@
 // Result of alignment
 use crate::{Deserialize, Serialize};
-#[cfg(feature = "short_key")]
-use serde::ser::{Serializer, SerializeStruct};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize, Deserialize)]
 pub struct AlignmentResult(
     pub Vec<RecordAlignmentResult>
 );
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Deserialize)]
-#[cfg_attr(not(feature = "short_key"), derive(Serialize))]
-
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "short_key", serde(rename = "RecAln"))]
 pub struct RecordAlignmentResult {
+    #[cfg_attr(feature = "short_key", serde(rename = "idx"))]
     pub index: usize,
+    #[cfg_attr(feature = "short_key", serde(rename = "aln"))]
     pub alignments: Vec<AnchorAlignmentResult>,
 }
-#[cfg(feature = "short_key")]
-impl Serialize for RecordAlignmentResult {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
-        S: Serializer,
-    {
-        let mut alignment = serializer.serialize_struct("RecAln", 2)?;
-        alignment.serialize_field("idx", &self.index)?;
-        alignment.serialize_field("aln", &self.alignments)?;
-        alignment.end()
-    }
-}
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Deserialize)]
-#[cfg_attr(not(feature = "short_key"), derive(Serialize))]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "short_key", serde(rename = "AncAln"))]
 pub struct AnchorAlignmentResult {
+    #[cfg_attr(feature = "short_key", serde(rename = "pen"))]
     pub penalty: usize,
+    #[cfg_attr(feature = "short_key", serde(rename = "len"))]
     pub length: usize,
+    #[cfg_attr(feature = "short_key", serde(rename = "pos"))]
     pub position: AlignmentPosition,
+    #[cfg_attr(feature = "short_key", serde(rename = "ops"))]
     pub operations: Vec<AlignmentOperation>,
 }
-#[cfg(feature = "short_key")]
-impl Serialize for AnchorAlignmentResult {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
-        S: Serializer,
-    {
-        let mut alignment = serializer.serialize_struct("AncAln", 4)?;
-        alignment.serialize_field("pen", &self.penalty)?;
-        alignment.serialize_field("len", &self.length)?;
-        alignment.serialize_field("pos", &self.position)?;
-        alignment.serialize_field("ops", &self.operations)?;
-        alignment.end()
-    }
-}
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Deserialize)]
-#[cfg_attr(not(feature = "short_key"), derive(Serialize))]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Serialize, Deserialize)]
 pub struct AlignmentPosition {
+    #[cfg_attr(feature = "short_key", serde(rename = "rec"))]
     pub record: (usize, usize),
+    #[cfg_attr(feature = "short_key", serde(rename = "qry"))]
     pub query: (usize, usize),
 }
-#[cfg(feature = "short_key")]
-impl Serialize for AlignmentPosition {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
-        S: Serializer,
-    {
-        let mut position = serializer.serialize_struct("Pos", 2)?;
-        position.serialize_field("rec", &self.record)?;
-        position.serialize_field("qry", &self.query)?;
-        position.end()
-    }
-}
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Deserialize)]
-#[cfg_attr(not(feature = "short_key"), derive(Serialize))]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "short_key", serde(rename = "Operation"))]
 pub struct AlignmentOperation {
+    #[cfg_attr(feature = "short_key", serde(rename = "op"))]
     pub case: AlignmentCase,
+    #[cfg_attr(feature = "short_key", serde(rename = "n"))]
     pub count: u32,
 }
-#[cfg(feature = "short_key")]
-impl Serialize for AlignmentOperation {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
-        S: Serializer,
-    {
-        let mut operation = serializer.serialize_struct("Operation", 2)?;
-        operation.serialize_field("op", &self.case)?;
-        operation.serialize_field("n", &self.count)?;
-        operation.end()
-    }
-}
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Deserialize)]
-#[cfg_attr(not(feature = "short_key"), derive(Serialize))]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Serialize, Deserialize)]
 pub enum AlignmentCase {
+    #[cfg_attr(feature = "short_key", serde(rename = "M"))]
     Match,
+    #[cfg_attr(feature = "short_key", serde(rename = "S"))]
     Subst,
+    #[cfg_attr(feature = "short_key", serde(rename = "I"))]
     Insertion,
+    #[cfg_attr(feature = "short_key", serde(rename = "D"))]
     Deletion,
-}
-#[cfg(feature = "short_key")]
-impl Serialize for AlignmentCase {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
-        S: Serializer,
-    {
-        match *self {
-            Self::Match => serializer.serialize_unit_variant("Case", 0, "M"),
-            Self::Subst => serializer.serialize_unit_variant("Case", 1, "S"),
-            Self::Insertion => serializer.serialize_unit_variant("Case", 2, "I"),
-            Self::Deletion => serializer.serialize_unit_variant("Case", 3, "D"),
-        }
-    }
 }
