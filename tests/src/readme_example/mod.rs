9e23db8693f4898bdd280147d7b80f85b1861051
baku4 <bahkhun@gmail.com>
2023-04-26 20:11:56 +0900

    Modify: version information and readme

---
+++ tests/src/readme_example/mod.rs
@@ -0,0 +1,29 @@
+#[test]
+fn quick_start() {
+    use sigalign::wrapper::{
+        DefaultAligner,
+        DefaultReference,
+    };
+
+    // (1) Build `Reference`
+    let fasta =
+    br#">record_1
+    ACACAGATCGCAAACTCACAATTGTATTTCTTTGCCACCTGGGCATATACTTTTTGCGCCCCCTCATTTA
+    >record_2
+    TCTGGGGCCATTGTATTTCTTTGCCAGCTGGGGCATATACTTTTTCCGCCCCCTCATTTACGCTCATCAC"#;
+    let reference = DefaultReference::from_fasta_bytes(fasta).unwrap();
+
+    // (2) Make `Aligner`
+    let mut aligner = DefaultAligner::new_local(
+        4,   // Mismatch penalty
+        6,   // Gap-open penalty
+        2,   // Gap-extend penalty
+        50,  // Minimum aligned length
+        0.2, // Maximum penalty per length
+    ).unwrap();
+
+    // (3) Align query to reference
+    let query = b"CAAACTCACAATTGTATTTCTTTGCCAGCTGGGCATATACTTTTTCCGCCCCCTCATTTAACTTCTTGGA";
+    let result = aligner.align_query(&reference, query).unwrap();
+    println!("{}", result.to_json());
+}
