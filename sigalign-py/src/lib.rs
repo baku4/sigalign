5fb7ee96b2d60041e27e0f2efac44f9381f03a92
baku4 <bahkhun@gmail.com>
2024-07-21 14:48:15 +0900

    Init to update python version to 0.3.0

--- sigalign-py/src/lib.rs
+++ sigalign-py/src/lib.rs
@@ -1,18 +1,18 @@
 use pyo3::prelude::*;
 
-mod reference;
-mod results;
-mod aligner;
+// mod reference;
+// mod results;
+// mod aligner;
 
-use reference::PyReference;
-use aligner::PyAligner;
-use results::get_result_module;
+// use reference::PyReference;
+// use aligner::PyAligner;
+// use results::get_result_module;
 
 #[pymodule]
-fn sigalign(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<PyReference>()?;
-    m.add_class::<PyAligner>()?;
-    let results_module = get_result_module(py)?;
-    m.add_submodule(results_module)?;
+fn sigalign(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // m.add_class::<PyReference>()?;
+    // m.add_class::<PyAligner>()?;
+    // let results_module = get_result_module(py)?;
+    // m.add_submodule(results_module)?;
     Ok(())
 }
