3383aa4eba4373b0facd973ff321e392ea7bd47e
baku4 <bahkhun@gmail.com>
2024-07-31 19:53:58 +0900

    Use thread pool to perform alingment

---
+++ examples/read-mapper/src/alignment/query_reader/mod.rs
@@ -0,0 +1,68 @@
+use std::{fs::File, io::Read, path::PathBuf};
+
+use crate::{Result, error, error_msg};
+
+use sigalign_utils::{
+    sequence_manipulation::reverse_complementary::reverse_complement_of_dna_sequence_in_place,
+    sequence_reader::{
+        decompress::get_gzip_decoder, fasta::FastaReader, fastq::FastqReader, IdRecord, IdRefRecord as _, SeqRecord as _
+    },
+};
+
+pub enum QueryReader {
+    Fasta(FastaReader<Box<dyn Read + Send>>),
+    Fastq(FastqReader<Box<dyn Read + Send>>),
+}
+
+impl QueryReader {
+    pub fn new(
+        file_path: &PathBuf,
+        is_gzip_compressed: bool,
+        is_fasta_file: bool,
+    ) -> Result<Self> {
+        let read: Box<dyn Read + Send> = {
+            let file = File::open(file_path)?;
+            if is_gzip_compressed {
+                Box::new(get_gzip_decoder(file))
+            } else {
+                Box::new(file)
+            }
+        };
+
+        if is_fasta_file {
+            Ok(Self::Fasta(FastaReader::new(read)))
+        } else {
+            Ok(Self::Fastq(FastqReader::new(read)))
+        }
+    }
+    pub fn fill_record_buffer(
+        &mut self,
+        sequence_buffer: &mut Vec<u8>,
+        label_buffer: &mut String,
+    ) -> Result<()> {
+        match self {
+            Self::Fasta(reader) => {
+                if let Some(mut record) = reader.next() {
+                    sequence_buffer.clear();
+                    label_buffer.clear();
+                    record.extend_seq_buf(sequence_buffer);
+                    record.extend_id_string(label_buffer)?;
+                    Ok(())
+                } else {
+                    Err(error!("No more records"))
+                }
+            },
+            Self::Fastq(reader) => {
+                if let Some(mut record) = reader.next() {
+                    sequence_buffer.clear();
+                    label_buffer.clear();
+                    record.extend_seq_buf(sequence_buffer);
+                    record.extend_id_string(label_buffer)?;
+                    Ok(())
+                } else {
+                    Err(error!("No more records"))
+                }
+            },
+        }
+    }
+}
